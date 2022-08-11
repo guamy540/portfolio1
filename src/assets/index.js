d270cb0b523793c6cc03740fc0b8e71af1d05ba6
guamy540 <49961360+guamy540@users.noreply.github.com>
2022-08-11 12:22:33 -0700

    about fixing

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,7 @@
 export {default as logo} from './images/logo2.png'
 export {default as email} from './images/email.png'
 export {default as mobile} from './images/mobile.png'
-export {default as profile} from './images/profile500.webp'
+export {default as profile} from './images/profile.webp'
 export {default as circle} from './images/circle.svg'
 export {default as node} from './images/nodejs20.webp'
 export {default as javascript} from './images/javascript.png'
