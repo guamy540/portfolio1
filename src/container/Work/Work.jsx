cfbd1a1e9ac5ac8ceea89857994ee4ab793891f9
guamy540 <49961360+guamy540@users.noreply.github.com>
2022-12-01 21:34:58 -0800

    eddited portfolio selection options

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -42,7 +42,7 @@ const Work = () => {
       <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
       <div className="app__work-filter">
-        {[ 'All', 'Shops', 'UI/UX', 'Professional Portfolios', 'Games'].map((item, index) => (
+        {[ 'All', 'UI/UX', 'Professional Portfolios', 'Games'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
